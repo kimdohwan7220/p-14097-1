308a0ee86793eb11f86dae707a5daf3a872dace1
kimdohwan7220 <dohwan8220@naver.com>
2025-11-26 10:52:31 +0900

    6 : Component 사용

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -1,6 +1,5 @@
 package com.back;
 
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,15 +8,5 @@ public class AppConfig {
     @Bean
     int version() {
         return 55;
-    }
-
-    @Bean
-    public ApplicationRunner myApplicationRunner() {
-        return new MyApplicationRunner();
-    }
-
-    @Bean
-    public ApplicationRunner myApplicationRunner2() {
-        return new MyApplicationRunner();
     }
 }
