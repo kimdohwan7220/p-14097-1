c1e63b29b81fcc2512c87fa31aeb5852601c9bef
kimdohwan7220 <dohwan8220@naver.com>
2025-11-26 10:51:02 +0900

    5 : Bean의 2개 사용

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -1,8 +1,23 @@
 package com.back;
 
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+    @Bean
+    int version() {
+        return 55;
+    }
 
+    @Bean
+    public ApplicationRunner myApplicationRunner() {
+        return new MyApplicationRunner();
+    }
+
+    @Bean
+    public ApplicationRunner myApplicationRunner2() {
+        return new MyApplicationRunner();
+    }
 }
